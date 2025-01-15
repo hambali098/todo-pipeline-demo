c62e9c56cab8bbd3ebf046dc8aa9c0158bb771ef
hambali098 <hambalitubagus@gmail.com>
2025-01-16 06:22:44 +0700

    edit docker

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-         ayo mulaiiii melangkah bersama ku{" "}
+         ayo mulaiiii melangkah bersamssa ku{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.tsx
             </code>
